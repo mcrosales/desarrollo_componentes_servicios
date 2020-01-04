a18941f80b62c52965ff24b1a6dc9d5eecde8782
mario_rosales <mariocrosl@gmail.com>
2020-01-04 23:24:34 +0100

    p1

---
+++ p1/src/main/java/prueba/HolaMundo.java
@@ -0,0 +1,15 @@
+package prueba;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="holaMundo", eager=true)
+@RequestScoped
+public class HolaMundo {
+	
+	public HolaMundo(){
+		System.out.println("Hola Mundo ha comenzado!");
+	}
+	public String getMessage(){
+		return "Hola Mundo";
+	}
+}
