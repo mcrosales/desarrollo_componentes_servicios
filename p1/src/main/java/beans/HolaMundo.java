f24ecc4c05937552b64c5398ed6abbec0cc065a9
mario_rosales <mariocrosl@gmail.com>
2020-01-04 23:30:41 +0100

    second

--- p1/src/main/java/beans/HolaMundo.java
+++ p1/src/main/java/beans/HolaMundo.java
@@ -1,4 +1,4 @@
-package prueba;
+package beans;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
--- p1/src/main/java/beans/Mensaje.java
+++ p1/src/main/java/beans/Mensaje.java
@@ -1,4 +1,4 @@
-package prueba;
+package beans;
 
 public class Mensaje {
 
