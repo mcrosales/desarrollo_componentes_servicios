9bea18e3869cf6e31464a85e208ab2688bf93f08
mario_rosales <mariocrosl@gmail.com>
2020-01-30 20:28:02 +0100

    p2

---
+++ p2/rest_api/src/resource/Test.java
@@ -0,0 +1,17 @@
+package resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/test")
+public class Test {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getTestService() {
+	return "Hello World! This is coming from webservice!!";
+	}
+
+}
